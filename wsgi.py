05f2b355afc44eae6d688dbdd601ce28ea61dc3f
uffsuru <editingdks@gmail.com>
2025-09-20 14:35:43 +0530

    Initial commit: Add AuctionHub project files

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,2 @@
-from app import socketio
-
-# The 'application' variable is what Gunicorn and other WSGI servers look for by default.
-# We point it to our socketio object, which wraps the Flask app and handles real-time events.
-application = socketio
+from app import app
+application = app
